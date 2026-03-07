49becc8bc8c179020a377a765bf96263309da8b1
Grischke <tonygrischke@gmail.com>
2026-03-06 22:21:13 -0500

    Fix syntax error in maven repository block

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,10 +8,10 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        google()
-        mavenCentral()
-        	maven { url = uri("https://jitpack.io") 
-    }
+    google()
+    mavenCentral()
+    maven { url = uri("https://jitpack.io") }
+}
 }
 
 rootProject.name = "CodeMasterAIStudio"
