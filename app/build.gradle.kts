c4e9c7bb40139897e7dcdae4afdfa594afdb0ae2
Grischke <tonygrischke@gmail.com>
2026-03-07 23:42:04 -0500

    fix: upgrade Hilt 2.50 -> 2.51.1 to fix KSP FileAlreadyExistsException

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -87,8 +87,8 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
 
     // Hilt DI
-    implementation("com.google.dagger:hilt-android:2.50")
-    ksp("com.google.dagger:hilt-android-compiler:2.50")
+    implementation("com.google.dagger:hilt-android:2.51.1")
+    ksp("com.google.dagger:hilt-android-compiler:2.51.1")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 
     // Room Database
